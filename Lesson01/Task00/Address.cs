9dafc9044e1a0bd3800c06a84faece611093fb9b
vatanov <atanov.v@gmail.com>
2018-10-23 23:16:06 +0300

    Task00 is done.

---
+++ Lesson01/Task00/Address.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Task00
+{
+    class Address
+    {
+        //#region Fields
+        //int index;
+        //string country, city, street, house, apartment;
+        //#endregion
+
+        #region Properties
+        public int Index { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string House { get; set; }
+        public string Apartment { get; set; }
+        #endregion
+
+        public void Print()
+        {
+            Console.WriteLine("Index: " + this.Index);
+            Console.WriteLine("Country: " + this.Country);
+            Console.WriteLine("City: " + this.City);
+            Console.WriteLine(this.Street + " street, " + this.House + ", apartment " + this.Apartment);
+        }
+    }
+}
---
+++ Lesson01/Task00/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task00
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Address myAddr = new Address();
+            myAddr.Index = 1234;
+            myAddr.Country = "Spain";
+            myAddr.City = "Madrid";
+            myAddr.Street = "Sunny";
+            myAddr.House = "221-B";
+            myAddr.Apartment = "64";
+
+            myAddr.Print();
+
+            Console.ReadKey();
+        }
+    }
+}
