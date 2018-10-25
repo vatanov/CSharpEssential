0c0c13daff29621759820f9b9b25cb0ebce020d3
Vladimir Atanov <atanov.v@gmail.com>
2018-10-25 17:35:47 +0300

    Task02 is done.

---
+++ Lesson01/Task02/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Task02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter side 1:\t");
+            double side1;
+            Double.TryParse(Console.ReadLine(), out side1);
+
+            Console.Write("Enter side 2:\t");
+            double side2;
+            Double.TryParse(Console.ReadLine(), out side2);
+
+            Rectangle rec = new Rectangle(side1, side2);
+
+            Console.WriteLine(new string ('-', 20));
+            Console.WriteLine("Perimeter:\t" + rec.Perimeter);
+            Console.WriteLine("Area:\t\t" + rec.Area);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lesson01/Task02/Rectangle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task02
+{
+    class Rectangle
+    {
+        double side1 { get; set; }
+        double side2 { get; set; }
+        public double Area
+        {
+            get { return this.AreaCalc(); }
+        }
+        public double Perimeter
+        {
+            get { return this.PerimeterCalc(); }
+        }
+        
+        public Rectangle(double side1, double side2)
+        {
+            this.side1 = side1;
+            this.side2 = side2;
+        }
+
+        public double AreaCalc()
+        {
+            return this.side1 * this.side2;
+        }
+
+        public double PerimeterCalc()
+        {
+            return this.side1 + this.side2 + this.side1 + this.side2;
+        }
+    }
+}
