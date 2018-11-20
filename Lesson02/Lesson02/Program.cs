282fc81dee06206cb65de546e32612c9532ff7b0
vatanov <atanov.v@gmail.com>
2018-11-20 23:02:46 +0200

    Lesson 02 Task 00 is done.

---
+++ Lesson02/Lesson02/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User usr = new User("Vampire", "Vovann", "Vovanoff", 30);
+            usr.ShowUserInfo();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lesson02/Lesson02/User.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson02
+{
+    class User
+    {
+        #region Fields
+
+        string login;
+        string name;
+        string familyName;
+        int age;
+        readonly DateTime date;
+
+        #endregion
+
+        #region Constructors
+
+        public User(string login, string name, string familyName, int age)
+        {
+            this.login = login;
+            this.name = name;
+            this.familyName = familyName;
+            this.age = age;
+            this.date = DateTime.Now;
+        }
+
+        #endregion
+
+        public void ShowUserInfo()
+        {
+            Console.WriteLine("User info:");
+            Console.WriteLine("Login:\t\t" + this.login);
+            Console.WriteLine("Name:\t\t" + this.name);
+            Console.WriteLine("Surname:\t" + this.familyName);
+            Console.WriteLine("Age:\t\t" + this.age);
+            Console.WriteLine("Registered:\t" + this.date);
+        }
+    }
+}
